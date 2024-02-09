9e83c4793ce8e4d4b087914b44b8b68ee506d2dd
Adem Şerif Dur <85637636+Ademseriff@users.noreply.github.com>
2024-02-10 01:07:08 +0300

    Add project files.

---
+++ FrontEndHomePage/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using FrontEndHomePage.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FrontEndHomePage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
+
+        public IActionResult About()
+        {
+            return View();
+        }
+     
+        public IActionResult Sepet(int id)
+        {
+            
+            return View();
+        }
+
+        public IActionResult SepetProcess(int id)
+        {
+            int Catagory,Price;
+            Catagory = id % 10;
+            Price = id / 10;
+
+            return RedirectToAction(actionName:"Menu",controllerName:"Home");
+        }
+
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
